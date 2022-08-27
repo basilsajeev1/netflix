d8757bb962c88867a3bade1cd658c646ea0b8537
Basil Sajeev <basilsajeev1@gmail.com>
2022-08-27 15:56:07 +0530

    Loaded banner and its contents dynamically

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,16 +1,30 @@
-import React from 'react'
+
+import React, { useEffect, useState } from 'react'
+import { API_KEY, imageUrl } from '../../constants/constants'
 import './Banner.css'
+import  axios from '../../axios'
+
+
 
 function Banner() {
+  let x = Math.floor((Math.random() * 19));
+  useEffect(()=>{
+    
+    axios.get(`trending/all/day?api_key=${API_KEY}`).then((response)=>{
+      console.log(response.data)
+      setTrending(response.data.results[x])
+    })
+  },[])
+  const [trending, setTrending] = useState()
   return (
-    <div className='banner'>
+    <div className='banner' style={{backgroundImage: `url(${trending ?imageUrl+trending.backdrop_path:""})`}}>
       <div className='bannerContent'>
-        <h2>Money Heist</h2>
+        <h2>{trending ? trending.title : ""}</h2>
         <div className="bannerButtons">
         <button className='bannerButton'>Play</button>
         <button className='bannerButton'>My List</button>
         </div>
-        <h5 className='bannerDescription'>In publishing and graphic design, Lorem ipsum is a placeholder text commonly used to demonstrate the visual form of a document or a typeface without relying on meaningful content.</h5>
+        <h5 className='bannerDescription'>{trending ? trending.overview : ""}</h5>
       </div>
       <div className='fade'></div>
     </div>
---
+++ src/constants/constants.js
@@ -0,0 +1,3 @@
+export const baseUrl= 'https://api.themoviedb.org/3/'
+export const API_KEY= '86210093dc092c63cdacd88bdb7b92e2'
+export const imageUrl='https://image.tmdb.org/t/p/w500/'
